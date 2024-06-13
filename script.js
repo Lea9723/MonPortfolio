e356ed08c7b71eb5cc0c1c814fecd2e4f8cb0e2d
Lea9723 <leastattner@gmail.com>
2024-06-13 19:42:10 +0200

    finalising2

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    const typewriter = document.getElementById('typewriter');
+    const options = {
+        root: null, // use the viewport
+        rootMargin: '0px',
+        threshold: 0.8 // trigger when at least 80% of the section is visible
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                typewriter.style.animation = "none";
+                setTimeout(()=>{
+                    typewriter.style.animation = 'typing 3.5s steps(40, end), blink-caret .5s step-end infinite, hide-cursor 3.5s 3.5s forwards';
+                 }, 30);
+             }
+
+        });
+    }, options);
+
+    observer.observe(typewriter);
+});
